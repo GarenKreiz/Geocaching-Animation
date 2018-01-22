5bf3e81b3cb2905c7c314ffbbe3bc56b8d19a18f
GarenKreiz <garenkreiz@gmail.com>
2018-01-22 17:13:50 +0100

    Fix typo in license

--- generateAnimation.py
+++ generateAnimation.py
@@ -39,7 +39,7 @@
 #
 # Licence:
 #   This program is free software: you can redistribute it and/or modify
-#   it under the terms of the GNU General Pu1blic License as published by
+#   it under the terms of the GNU General Public License as published by
 #   the Free Software Foundation, either version 3 of the License, or
 #   (at your option) any later version.
 #
