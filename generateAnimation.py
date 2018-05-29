ceb4bf3c8876387484f111c3c4ec69c447feb59b
GarenKreiz <garenkreiz@gmail.com>
2018-05-29 10:49:54 +0200

    Compute distance with Vincenty Inverse algorithm instead of Haversine

--- generateAnimation.py
+++ generateAnimation.py
@@ -247,7 +247,64 @@ def fileFindNext(f,pattern):
 
 # compute distance between two points on Earth surface
 
-def getDistance(lat1, lng1, lat2, lng2):
+def getDistance(lat1,lng1,lat2,lng2,maxIter=200,tol=10**-12):
+
+  # algorithm Vincenty Inverse (source http://nathanrooy.github.io/ )
+
+  if ((lat1,lng1) == (lat2,lng2)):
+    return 0.0
+
+  #--- CONSTANTS ------------------------------------+
+  a=6378137.0                             # radius at equator in meters (WGS-84)
+  f=1/298.257223563                       # flattening of the ellipsoid (WGS-84)
+  b=(1-f)*a
+
+  phi_1,L_1,=lat1,lng1                    # (lat=L_?,lon=phi_?)
+  phi_2,L_2,=lat2,lng2
+
+  u_1=math.atan((1-f)*math.tan(math.radians(phi_1)))
+  u_2=math.atan((1-f)*math.tan(math.radians(phi_2)))
+
+  L=math.radians(L_2-L_1)
+
+  Lambda=L                                # set initial value of lambda to L
+
+  sin_u1=math.sin(u_1)
+  cos_u1=math.cos(u_1)
+  sin_u2=math.sin(u_2)
+  cos_u2=math.cos(u_2)
+
+  #--- BEGIN ITERATIONS -----------------------------+
+  iters=0
+  for i in range(0,maxIter):
+    iters+=1
+
+    cos_lambda=math.cos(Lambda)
+    sin_lambda=math.sin(Lambda)
+    sin_sigma=math.sqrt((cos_u2*math.sin(Lambda))**2+(cos_u1*sin_u2-sin_u1*cos_u2*cos_lambda)**2)
+    cos_sigma=sin_u1*sin_u2+cos_u1*cos_u2*cos_lambda
+    sigma=math.atan2(sin_sigma,cos_sigma)
+    sin_alpha=(cos_u1*cos_u2*sin_lambda)/sin_sigma
+    cos_sq_alpha=1-sin_alpha**2
+    cos2_sigma_m=cos_sigma-((2*sin_u1*sin_u2)/cos_sq_alpha)
+    C=(f/16)*cos_sq_alpha*(4+f*(4-3*cos_sq_alpha))
+    Lambda_prev=Lambda
+    Lambda=L+(1-C)*f*sin_alpha*(sigma+C*sin_sigma*(cos2_sigma_m+C*cos_sigma*(-1+2*cos2_sigma_m**2)))
+
+    # successful convergence
+    diff=abs(Lambda_prev-Lambda)
+    if diff<=tol:
+      break
+
+  u_sq=cos_sq_alpha*((a**2-b**2)/b**2)
+  A=1+(u_sq/16384)*(4096+u_sq*(-768+u_sq*(320-175*u_sq)))
+  B=(u_sq/1024)*(256+u_sq*(-128+u_sq*(74-47*u_sq)))
+  delta_sig=B*sin_sigma*(cos2_sigma_m+0.25*B*(cos_sigma*(-1+2*cos2_sigma_m**2)-(1/6)*B*cos2_sigma_m*(-3+4*sin_sigma**2)*(-3+4*cos2_sigma_m**2)))
+
+  return b*A*(sigma-delta_sig) / 1000       # output distance in kilometers
+
+
+def getDistanceHaversine(lat1, lng1, lat2, lng2):
 
   # algorithm Haversine
   #
